c9ba23050f4643d2bcef92f05fdf460b99dc461c
Benoit Tigeot <benoit@hopsandfork.com>
2021-05-26 23:46:12 +0200

    Remove fontawesome

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,1 @@
 import 'bootstrap';
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
-
-// import 'font-awesome/css/font-awesome.css';
-
-$(document).ready(function () {
-    console.log('Loaded!');
-});
